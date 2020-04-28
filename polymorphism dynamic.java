303a29197d7ae13a37a5e86c99ab814b8d9197de
YanggaCS <yanggacs@gmail.com>
2020-04-28 14:46:48 +0700

    Update polymorphism dynamic.java

--- polymorphism dynamic.java
+++ polymorphism dynamic.java
@@ -1,10 +1,15 @@
+package polymorphism;
+// Membuat class induk dengan nama hewan
 class Hewan {
+    //Membuat method void suara dengan modifier public
     public void suara() {
         System.out.println("....");
     }
 }
+//Membuat class kucing yeng merupakan turunan dari class hewan
 class Kucing extends Hewan {
-     @Override
+    //Membuat method overiding
+    @Override
     public void suara() {
         System.out.println("Meong");
     }
@@ -13,8 +18,7 @@ class Anjing extends Hewan {
     @Override
     public void suara() {
         System.out.println("Gukguk");
-    }
-    
+    }  
 }
 class Kambing extends Hewan {
     @Override
@@ -22,15 +26,16 @@ class Kambing extends Hewan {
         System.out.println("Mbekk");
     }
 }
-
-class bunyi {
+//Main program
+public class Polymorphism {
     public static void main(String args[ ]) {
+        //Membuatembuat objek baru dengan nama a, b, dan c
         Hewan a = new Anjing();
         Hewan b = new Kucing();
         Hewan c = new Kambing();
-        
+        //Mengakses method suara dari masing-masing object
         a.suara();
         b.suara();
         c.suara();
-    }
-}
+    }  
+}   
