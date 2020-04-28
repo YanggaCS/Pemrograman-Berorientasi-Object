8d0e26e6b530c593da0aa894101b80ff4aba8964
YanggaCS <yanggacs@gmail.com>
2020-04-28 15:13:51 +0700

    Update polymorphism static.java

--- polymorphism static.java
+++ polymorphism static.java
@@ -1,22 +1,22 @@
+package polymorphism;
+//Membuat class induk tampilan
 class tampilkan{
-  
+   //membuat method void suara dengan modifier public
    public void print(int angka){
      System.out.println("ini adalah angka :" + angka);
    }
-   
+   //membuat method yang sama tetapi berbeda parameter 
    public void print(String string){
      System.out.println("ini adalah string:" + string);
    }
-    
 }
 
-public class main{
+public class Polymorphism{
   public static void main(String args[]){
+    //membuat objek baru
     tampilkan menampilkan= new tampilkan();
-    
+    //Memanggil method print memasukkan parameter yang berbeda
     menampilkan.print("halo");
     menampilkan.print(10);
-    
-     
    }
-}
+} 
